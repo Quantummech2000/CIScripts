cf1f0d459dfb314a9abd8fb1c67d00b2788e0a1d
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2018-11-25 19:16:15 +0530

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -11,8 +11,8 @@ cd rom
 git config —global user.name "Quantummech2000"
 git config —global user.email "vvr-rockstar@bmsit.in"
 repo init -u https://github.com/LLuvia-OS/android_manifest -b 3.0 --depth=1
-repo sync -c -f --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 y
+repo sync -c -f --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 git clone https://github.com/quantummech2000/android_device_xiaomi_santoni -b lluvia-pie device/xiaomi/santoni
 git clone https://github.com/quantummech2000/android_vendor_xiaomi_santoni -b lluvia-pie vendor/xiaomi
 git clone https://github.com/quantummech2000/jaguarkernel.git -b version2.0_treble kernel/xiaomi/msm8937
