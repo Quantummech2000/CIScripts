4b1a562e8a5fb1a46680a3bcf4bf12e866eee0a8
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2018-12-08 10:25:47 +0530

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+export TZ="Asia/Kolkata";
+export WKDIR="${HOME}/rom"
+export ROM="${WKDIR}/out/target/product/santoni/LLuvia* "
+
+
 git clone https://github.com/akhilnarang/scripts
 cd scripts
 bash setup/a*b*e*.sh
@@ -23,3 +30,24 @@ export USE_CCACHE=1
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
 export CCACHE_COMPRESS=1
 brunch santoni
+
+if [[ ! -f "${IMAGE}" ]]; then
+    echo -e "Build failed :P";
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="CI build for LLUVIA ROM stopped due to an error" -d chat_id=$CHAT_ID
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker -d sticker="CAADBQADHwADx8M3DyJi1SWaX6BdAg"  -d chat_id=$CHAT_ID
+    success=false;
+    exit 1;
+else
+    echo -e "Build Succesful!";
+    success=true;
+fi
+function transfer() {
+	zipname="${ROM}";
+	url="$(curl -# -T ${ROM} https://transfer.sh)";
+	printf '\n';
+	echo -e "Download ${zipname} at ${url} ";
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$url" -d chat_id=$CHAT_ID
+}
+if [[ ${success} == true ]]; then
+    echo -e "Uploading ${ZIPNAME} to https://transfer.sh/";
+    transfer "${FINAL_ZIP}";
