d91676cb8cace4948efe6d30e70d763599fc3c66
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2019-07-04 15:29:09 +0530

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,14 @@
+echo "***Auto Compiler Updater***"
+apt update > /dev/null 2>&1
+apt upgrade -y > /dev/null 2>&1
+apt install python3 -y > /dev/null 2>&1
+python3 strip.py
+chmod 777 telegram
+chmod 777 telegramapi
+apt install sudo git lsb-core apt-utils -y > /dev/null 2>&1
+echo 'Initial Dependencies Installed......'
+sudo echo "ci ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd -m -d /home/ci ci
+useradd -g ci wheel
+echo `pwd` > /tmp/loc
+sudo -Hu ci bash -c "bash runner_user.sh"
