1310feeb915594fa0c6b96d46615aba43e1778e0
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2019-07-04 15:26:04 +0530

    Create Build.sh

---
+++ Build.sh
@@ -0,0 +1,52 @@
+build_env()
+{
+. "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
+TELEGRAM_TOKEN=$(cat /tmp/tg_token)
+export TELEGRAM_TOKEN
+tg_sendinfo "<code>[BoT]: Meh new build is Starting!</code>"
+cd ~
+git clone https://github.com/akhilnarang/scripts > /dev/null 2>&1
+cd scripts
+bash setup/android_build_env.sh  > /dev/null 2>&1
+cd ..
+rm -rf scripts
+
+echo "Build Dependencies Installed....."
+}
+##### Build Configs
+
+build_conf()
+{
+mkdir repo
+cd repo
+git config --global user.email "abc@gmail.com"
+git config --global user.name "ahemahem"
+git config --global color.ui false
+export LOC=$(cat /tmp/loc)
+}
+
+run()
+{
+echo "Starting build!"
+repo init --depth=1 -u https://github.com/ArrowOS/android_manifest.git -b arrow-9.x > /dev/null 2>&1
+repo sync  -f --force-sync --no-clone-bundle -j8 > /dev/null 2>&1
+git clone https://github.com/ArrowOS-Devices/android_device_asus_X00TD device/asus/X00TD
+. build/envsetup.sh 
+brunch X00TD > /dev/null 2>&1
+cd out/target/product/X00TD
+curl -F'file=@Arrow-v9.0-X00TD-OFFICIAL-*.zip' https://0x0.st
+
+echo "Build finished!"
+}
+
+##### try pushing
+push_gcc()
+{
+tg_sendinfo "<code>Check Out New Build ;_;</code>"
+echo "Job Successful!"
+}
+
+build_env
+build_conf
+run
+push_gcc
