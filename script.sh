d5ee7fd4f6929aaa0b3769b2119af2d3418cedfb
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2019-07-12 11:35:27 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+apt update > /dev/null 2>&1
+apt upgrade -y > /dev/null 2>&1
+apt install ccache bc libncurses5-dev git-core gnupg flex bison gperf build-essential zip curl libc6-dev ncurses-dev  > /dev/null 2>&1
+export BOT_API_KEY="673621974:AAGAEzCHFNn7e0KyGJVyJg01HBn29aWwKiw"
+cd
+export CHAT_ID="585730571"
+export ARCH=arm64
+export KERNELDIR=$HOME/WolfKernel
+export KERNELNAME=Wolf
+git clone https://github.com/quantummech2000/WolfKernel -b v1.9 WolfKernel
+wget https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+tar -xvf  gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+git clone https://github.com/VRanger/dragontc.git clang
+cd WolfKernel
+bash wolf.sh
