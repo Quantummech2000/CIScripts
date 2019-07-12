b8dd9e805a84d397acb3aaebc3457ce30c48d222
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2019-07-12 11:39:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-apt update > /dev/null 2>&1
-apt upgrade -y > /dev/null 2>&1
-apt install ccache bc libncurses5-dev git-core gnupg flex bison gperf build-essential zip curl libc6-dev ncurses-dev  > /dev/null 2>&1
+apt update 
+apt upgrade -y 
+apt install ccache bc libncurses5-dev git-core gnupg flex bison gperf build-essential zip curl libc6-dev ncurses-dev 
 export BOT_API_KEY="673621974:AAGAEzCHFNn7e0KyGJVyJg01HBn29aWwKiw"
 cd
 export CHAT_ID="585730571"
